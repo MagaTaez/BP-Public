bfe3a624deee84cc26a99beb41ffe02b9e8185fb
MagaTaez <a.artemsky@gmail.com>
2024-03-10 22:53:44 +0100

    Refactored custom exceptions

--- bp-backend/src/main/java/cz/vse/java/ploa00/bpbackend/exception/OrderStateException.java
+++ bp-backend/src/main/java/cz/vse/java/ploa00/bpbackend/exception/OrderStateException.java
@@ -1,6 +1,10 @@
 package cz.vse.java.ploa00.bpbackend.exception;
 
-public class OrderStateException extends Exception{
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class OrderStateException extends RuntimeException{
 
     public OrderStateException(String message) {super(message);}
 }
