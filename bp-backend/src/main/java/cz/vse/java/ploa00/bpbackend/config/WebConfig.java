2cb608ee571ccce55bf0c8e942bb00f5bb821ed2
MagaTaez <a.artemsky@gmail.com>
2024-04-22 23:17:11 +0200

    Changed servername for CORS to environment variable

--- bp-backend/src/main/java/cz/vse/java/ploa00/bpbackend/config/WebConfig.java
+++ bp-backend/src/main/java/cz/vse/java/ploa00/bpbackend/config/WebConfig.java
@@ -1,15 +1,21 @@
 package cz.vse.java.ploa00.bpbackend.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    Environment env;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints
-                .allowedOrigins("http://localhost:3000") // Allow this origin
+                .allowedOrigins(env.getProperty("bpIMS.allowedOrigins")) // Allow this origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // Allow these methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true);
